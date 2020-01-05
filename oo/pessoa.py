3a81fde0197651d07622dcb7e098427865c86f67
edmarcavalcante <eng.edmar@gmail.com>
2020-01-05 20:18:31 -0300

    criado método complementar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,10 @@
 class Pessoa:
-    pass
+    def cumprimenrar(self): #isso é um método. Método não passa de uma função no corpo de uma classe
+        return 'Olá'
+
+if __name__=='__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimenrar(p))
+    print(p.cumprimenrar()) # essa é a forma correta de chamar um objeto junto com um método
+
+
